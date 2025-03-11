13729003a6787ff50f7c85a4473d2e7dd03f5bfc
Svroozendaal <simonvRoozendaal@gmail.com>
2025-03-11 22:07:25 +0100

    Add Firebase configuration and analytics

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,19 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "pokerpal-online.firebaseapp.com",
-  projectId: "pokerpal-online",
-  storageBucket: "pokerpal-online.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDuviiD94sK5o9yHgbz-AnmjrEPy_YtAkM",
+  authDomain: "pokerpal-1176a.firebaseapp.com",
+  projectId: "pokerpal-1176a",
+  storageBucket: "pokerpal-1176a.firebasestorage.app",
+  messagingSenderId: "242856880102",
+  appId: "1:242856880102:web:0aa7b0fffce7efd751d827",
+  measurementId: "G-NT9PDB1NYK"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
 export default app; 
